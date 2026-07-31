b2c1ed002fe49b713f28d296e1c84db45b04fad7
smitsolanki4518-byte <smitsolanki4518@gmail.com>
2026-07-31 10:32:52 +0530

    Add files via upload

---
+++ U2(5).C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l,b,a;
+	clrscr();
+	printf("\n enter value of l:");
+	scanf("%d",&l);
+	printf("\n enter value of b:");
+	scanf("%d",&b);
+
+	a =l * b;
+	printf("\n area of rectangle is:%d",a);
+	getch();
+}
+
+
